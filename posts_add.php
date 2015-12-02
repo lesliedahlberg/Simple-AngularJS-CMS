4066c214c8eb6a0e1f636c16a0a9feeb6064c70b
lesliedahlberg <lesliedahlberg@Leslies-MacBook-Pro.local>
2015-12-02 22:48:45 +0100

    -

--- posts_add.php
+++ posts_add.php
@@ -3,6 +3,31 @@
 <? require $SETTINGS_php_include["header"]; ?>
 
 <section class="container">
+
+  <!-- Static navbar -->
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Project name</a>
+      </div>
+      <div id="navbar" class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+          <li><a href="posts.php">Posts</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a class="active" href="posts_add.php">Add Post</a></li>
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div><!--/.container-fluid -->
+  </nav>
+
+
 <script src="posts_add.js"></script>
 <h1>Add Post</h1>
 <p>{{ message }}</p>
